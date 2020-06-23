84d6b3a869747c791314d733ca420a9c287254bd
Peter Koenders <pkoenders@gmail.com>
2020-06-23 21:26:03 +1200

    aa

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
-        enableIdentityWidget: false,
+        enableIdentityWidget: true,
       }
     },
 
